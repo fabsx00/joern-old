958c33f8235e79bb98432046fd92345826ec967b
Fabian Yamaguchi <fabianyamaguchi@gmx.de>
2012-05-27 19:45:59 +0200

    Convinced. Renaming lain to joern prior to release ;)

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from distutils.core import setup
 
 files = ["sourceutils/*"]
 
-setup(name = "lain",
+setup(name = "joern",
     version = "0.0.1",
-    description = "Lain is a collection of command line tools enabling development of language aware code analysis tools for C/C++ even when only parts of the source code are available.",
+    description = "Joern is a collection of command line tools enabling development of language aware code analysis tools for C/C++ even when only parts of the source code are available.",
     author = "Fabian Yamaguchi",
     author_email = "fabs@phenoelit.de",
     url = "http://codeexploration.blogspot.de",
@@ -15,7 +15,7 @@ setup(name = "lain",
                 'sourceutils/pythonASTs', 'sourceutils/pythonCFGs'                
 ],
     package_data = {'sourceutils' : files , 'sourceutils/csvASTs' : ['CodeSensor.jar']},
-    scripts = ["lain_filter_asts", "lain_filter_cfgs", "lain_index", "lain_parse", "lain_plot"],
+    scripts = ["joern_filter_asts", "joern_filter_cfgs", "joern_index", "joern_parse", "joern_plot"],
     # long_description = """Really long text here.""" 
     
     #classifiers = []     
